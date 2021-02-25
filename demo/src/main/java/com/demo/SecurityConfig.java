a3fd6b557d0a43a0266cff2d59f4fe02861bea89
pablo cambra <lajotacambra2@gmail.com>
2021-02-25 12:12:47 +0100

    modificacion de SecurityConfig

--- demo/src/main/java/com/demo/SecurityConfig.java
+++ demo/src/main/java/com/demo/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.authorizeRequests().anyRequest().permitAll();
+		http.csrf().disable();
 		
 	}
 	
