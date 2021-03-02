6b8baa58d34c86664df20f2dd09525bea86065ab
pablo cambra <lajotacambra2@gmail.com>
2021-03-02 20:16:49 +0100

    Primera configuracion de Swagger

--- demo/src/main/java/com/demo/SecurityConfig.java
+++ demo/src/main/java/com/demo/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -47,13 +48,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 	       http.csrf().disable().authorizeRequests()
 	        .antMatchers("/").permitAll()
+	        .antMatchers("v2/api-docs").permitAll()
+	        .antMatchers("swagger-ui.html").permitAll()
 	        .antMatchers(HttpMethod.GET,"/api/all").permitAll()
 	        .antMatchers(HttpMethod.POST, "/api/save").permitAll()
 	        .antMatchers(HttpMethod.POST,"/newuser/*").permitAll()
 	        .antMatchers(HttpMethod.GET,"/master/*").permitAll()
 	         .antMatchers(HttpMethod.GET,"/exploreCourse").permitAll()
 	        .anyRequest().authenticated();
+	        
+	        
+	        
 	}
+	    
+	    
+	    @Override
+	    public void configure(WebSecurity web) throws Exception {
+	        web.ignoring().antMatchers("/v2/api-docs",
+	                                   "/configuration/ui",
+	                                   "/swagger-resources/**",
+	                                   "/configuration/security",
+	                                   "/swagger-ui.html",
+	                                   "/webjars/**");
+	    }
+	    
+	    
 
 	
 }
