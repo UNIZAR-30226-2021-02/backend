1ef42048376f4de09ddb329a8b33571482c55a9c
pablo cambra <lajotacambra2@gmail.com>
2021-03-22 12:20:43 +0100

    el script aparte de construir la imagen la sube a docker hub

--- demo/docker_build_push.sh
+++ demo/docker_build_push.sh
@@ -11,4 +11,7 @@ if [ $# -eq 1 ];then
 #construimos la app
 docker build -f Dockerfile -t proyectokalboware/app:v${version} .
 
+
+#hacemos push a docker hub
+docker push proyectokalboware/app:v${version}
 fi
