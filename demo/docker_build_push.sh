f30f7db98cee9b033878a954679187d972b929d2
pablo cambra <lajotacambra2@gmail.com>
2021-05-12 12:36:35 +0200

    dockercompose actualizado

--- demo/docker_build_push.sh
+++ demo/docker_build_push.sh
@@ -14,4 +14,10 @@ docker build -f Dockerfile -t proyectokalboware/app:v${version} .
 
 #hacemos push a docker hub
 docker push proyectokalboware/app:v${version}
+
+
+git add .
+git commit -m "dockercompose actualizado"
+git push
+ssh -n ${USER}@35.246.75.160 "cd ~/backend/demo;git pull;docker-compose down;docker-compose up"
 fi
