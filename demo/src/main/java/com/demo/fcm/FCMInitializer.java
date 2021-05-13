6a33752ba9f41d51e2417dd9b44b87c4eb222e3b
pablo cambra <lajotacambra2@gmail.com>
2021-05-13 10:59:46 +0200

    seguimos con notificaciones, compila pero no manda

--- demo/src/main/java/com/demo/fcm/FCMInitializer.java
+++ demo/src/main/java/com/demo/fcm/FCMInitializer.java
@@ -1,36 +1,42 @@
 package com.demo.fcm;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 
 import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+@Service
 public class FCMInitializer {
-    @Value("${app.firebase-configuration-file}")
-    private String firebaseConfigPath;
+	
+	
+	
+    private String firebaseConfigPath = "serviceAccountKey.json";
+
     
-    private FirebaseApp firebaseApp;
-    //@PostConstruct
+    @PostConstruct
     public void initialize() {
     	
         try {
-        	System.out.println("Llego, mi fichero es "+ firebaseConfigPath);
+        	System.out.println("El path es "+ firebaseConfigPath);
             FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())).build();
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())).build();
             if (FirebaseApp.getApps().isEmpty()) {
-                        FirebaseApp.initializeApp(options);
-            }
-            else {
-            	this.firebaseApp = FirebaseApp.getInstance();
+            	
+                FirebaseApp.initializeApp(options);
+                
             }
         } catch (IOException e) {
-                e.printStackTrace();
-        }
+            
     }
+
+}
 }
